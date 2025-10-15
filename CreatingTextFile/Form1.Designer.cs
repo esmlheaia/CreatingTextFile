ee31a92bf89ae7c4c3ab58eb2e07ee5f96e914c6
esmlheaia <miya.solenee@gmail.com>
2025-10-15 18:39:37 +0800

    UI Design FrmLab1

--- CreatingTextFile/Form1.Designer.cs
+++ CreatingTextFile/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CreatingTextFile
 {
-    partial class Form1
+    partial class FrmLab1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(44, 44);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(241, 80);
+            this.txtInput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(125, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmLab1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::CreatingTextFile.Properties.Resources.White_and_Beige_Minimalist_Aesthetic_New_in_my_Blog_Instagram_Post;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(328, 259);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "FrmLab1";
+            this.Text = "FrmLab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button1;
     }
 }
 
